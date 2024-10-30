1fa69e52c7eead4d9bb6bad42702685e8027dcda
Vremq_vperyod <df1050@126.com>
2024-10-30 16:10:06 +0800

    Sdk写好啦！

---
+++ PluginSdk/IFakeConsole.cs
@@ -0,0 +1,45 @@
+﻿using EleCho.GoCqHttpSdk.Message;
+
+namespace PluginSdk
+{
+    public interface IFakeConsole
+    {
+        public void Clear();
+
+        public void Close();
+
+        public Task<string> ReadLine();
+
+        public void Write(bool sth);
+
+        public void Write(char sth);
+
+        public void Write(char[] sth);
+
+        public void Write(char[] a, int b, int c);
+
+        public void Write(decimal sth);
+
+        public void Write(double sth);
+
+        public void Write(int sth);
+
+        public void Write(long sth);
+
+        public void Write(object sth);
+
+        public void Write(float sth);
+
+        public void Write(string sth);
+
+        public void Write(uint sth);
+
+        public void Write(ulong sth);
+
+        public void WriteLine();
+
+        public void WriteLine(string sth);
+
+        public void WriteMessage(CqMessage message);
+    }
+}
---
+++ PluginSdk/IPlugin.cs
@@ -0,0 +1,30 @@
+﻿using EleCho.GoCqHttpSdk;
+using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
+
+namespace PluginSdk
+{
+    internal interface IPlugin
+    {
+        public string Name { get; }
+
+        public string Description { get; }
+
+        public void ConfigLogger(ILoggerFactory loggerFactory);
+
+        public void ConfigStorage(ConcurrentDictionary<string, object> sharedStorage, ConcurrentDictionary<string, object> localStorage);
+
+        public void ConfigSession(ICqActionSession session);
+
+        public void Config();
+
+        public string CheckStartHandle { get; }
+
+        public string CheckStillHandle { get; }
+
+        public void Handler(IFakeConsole fakeConsole)
+        {
+
+        }
+    }
+}
