e5a48b840d0bddebd40496a5d0dc5bed937b6de7
Vremq_vperyod <df1050@126.com>
2024-11-04 19:39:42 +0800

    添加更多注释

--- PluginSdk/IFakeConsole.cs
+++ PluginSdk/IFakeConsole.cs
@@ -28,36 +28,55 @@ namespace PluginSdk
         /// </summary>
         public int Lines { get; }
 
+        /// <see cref="System.Text.StringBuilder.Append(bool)"/>
         public void Write(bool sth);
 
+        /// <see cref="System.Text.StringBuilder.Append(char)"/>
         public void Write(char sth);
 
+        /// <see cref="System.Text.StringBuilder.Append(char[])"/>
         public void Write(char[] sth);
 
+        /// <see cref="System.Text.StringBuilder.Append(char[], int, int)"/>
         public void Write(char[] a, int b, int c);
 
+        /// <see cref="System.Text.StringBuilder.Append(decimal)"/>
         public void Write(decimal sth);
 
+        /// <see cref="System.Text.StringBuilder.Append(double)"/>
         public void Write(double sth);
 
+        /// <see cref="System.Text.StringBuilder.Append(int)"/>
         public void Write(int sth);
 
+        /// <see cref="System.Text.StringBuilder.Append(long)"/>
         public void Write(long sth);
 
+        /// <see cref="System.Text.StringBuilder.Append(object)"/>
         public void Write(object sth);
 
+        /// <see cref="System.Text.StringBuilder.Append(float)"/>
         public void Write(float sth);
 
+        /// <see cref="System.Text.StringBuilder.Append(string)"/>
         public void Write(string sth);
 
+        /// <see cref="System.Text.StringBuilder.Append(uint)"/>
         public void Write(uint sth);
 
+        /// <see cref="System.Text.StringBuilder.Append(ulong)"/>
         public void Write(ulong sth);
 
+        /// <see cref="System.Text.StringBuilder.AppendLine()"/>
         public void WriteLine();
 
+        /// <see cref="System.Text.StringBuilder.AppendLine(string)"/>
         public void WriteLine(string sth);
 
+        /// <summary>
+        /// 发送原始消息
+        /// </summary>
+        /// <param name="message">要发送的消息</param>
         public void WriteMessage(CqMessage message);
     }
 }
